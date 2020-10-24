b80c3f6d3e58e8ff87cf05b487f3894a7c6a7a12
vcanadell <62500022+vcanadell@users.noreply.github.com>
2020-10-24 13:30:20 +0200

    Update settings.py
    Added  'vcanadell.pythonanywhere.com' as allowed hosts.

--- learning_templates/learning_templates/settings.py
+++ learning_templates/learning_templates/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'o0mwws#eqj+(n7ysn%!r*h0y-vwv$+pc*7@kpir^mhkmz_uu7b'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['vcanadell.pythonanywhere.com']
 
 
 # Application definition
